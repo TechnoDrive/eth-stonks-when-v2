40649d6f8cb6a1c78c0a7304d53d3f41069f1f0c
TheRealWendys <zarifabrar02@gmail.com>
2020-09-25 23:33:56 -0400

    Launch v2, fix minor bugs, make UI cleaner

---
+++ readme.md
@@ -0,0 +1,30 @@
+# eth-stonks-when
+
+## What is **EthStonksWhen**?
+EthStonksWhen is a command line application designed to alert the user (via a native Windows notification) when ETH (a cryptocurrency) reaches a certain value.
+
+## How do I use EthStonksWhen?
+After installing Node.js, and unzipping the file, run the following command in a windows cmd prompt (`WIN + S + "CMD" + ENTER`):
+`launch`
+This will install the needed modules, and run the app.
+
+## How do I install `Node.js`?
+1. Go to the [Node.js Website](https://nodejs.org/)
+2. Click the **"14.12.0"** download button.
+3. Run the installer. 
+
+**NOTE:** Make sure to click "Add to PATH" during the installation, so that you can use the command globally.
+
+![](../assets/nodejs-download.png)
+
+## Why not just make an executable?
+Some files in the module I'm using to send a notification cannot be packaged.
+
+## Supported Operating Systems?
+Should work on Windows, might work on Linux.
+Cross-platform **will** be added on v3 (as of right now it's v2).
+
+## Known Bugs
+At the moment, none.
+
+#### Made with love by Hextanium#5890 for the Salad community! (discord.gg/salad)
---
+++ src/index.js
@@ -0,0 +1,54 @@
+const Kraken = require('node-crypto-api').Kraken,
+    kraken = new Kraken();
+const notifier = require('node-notifier');
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+
+inquirer.prompt([{
+        type: 'input',
+        name: 'targetEthPrice',
+        message: "What price would you like to be notified on (in USD, for ETH)?",
+        validate: (value) => {
+            var test = Number(value);
+            if (test) {
+                return true;
+            } else {
+                console.log('Input failed the conversion test, do not enter "0" or a non-numeric value.')
+            }
+        }
+    }])
+    .then(answers => {
+        console.log(`Starting ${chalk.bold.white('EthStonksWhen')} to check the price of ${chalk.bold.white('ETH')} every ${chalk.bold.white('1 minute')}.`)
+        setInterval(() => {
+            kraken.ticker('ETH', 'USD')
+                .then(data => {
+                    let currentEthPrice = Number(result.result.XETHZUSD.o) // Gets the price of ETH
+                    let targetEthPrice = Number(answers.targetEthPrice);
+
+                    if (currentEthPrice == targetEthPrice) {
+                        notifier.notify({
+                                title: `ETH is Stonks!`,
+                                message: `ETH has reached ${targetEthPrice}!`,
+                                appID: 'Made by Hextanium#5890, for salad.io!',
+                                sound: true, // Only Notification Center or Windows Toasters
+                                wait: true // Wait with callback, until user action is taken against notification, does not apply to Windows Toasters as they always wait or notify-send as it does not support the wait option
+                            },
+                            function (err, response) {
+                                err ? console.error(err) : true;
+                                process.exit(0)
+                            }
+                        );
+                    }
+                })
+                .catch(error => {
+                    console.error(error)
+                });
+        }, 60 * 1000);
+    })
+    .catch(error => {
+        if (error.isTtyError) {
+            console.log('There was an error rendering the prompt in your terminal window.\nTry not to resize it during the prompts.')
+        } else {
+            console.log('There was an unknown error.')
+        }
+    });
